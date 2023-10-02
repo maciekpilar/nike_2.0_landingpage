07a1b1f2c816f1f7ecbbc27f7d86fa24429639ec
Maciej Pilarski <maciekpilar12@gmail.com>
2023-10-02 14:59:44 +0200

    Footer changes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -48,9 +48,10 @@ const Footer = () => {
       </div>
       <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
         <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+          <p> Copyright. All rights reserved.</p>
           <p>
-            Copyright Ⓒ 2023 | All Rights Reserved | Page created to practice
-            with{" "}
+            Copyright &copy; 2023 | All Rights Reserved | Page created to
+            practice with{" "}
             <a
               href="https://www.youtube.com/@javascriptmastery"
               className="hover:underline"
