e27a742ebef513a1934eb4e4b50d8e0e11bcb4fd
Maciej Pilarski <maciekpilar12@gmail.com>
2023-10-02 14:56:57 +0200

    Footer changes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -48,8 +48,7 @@ const Footer = () => {
       </div>
       <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
         <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
-          <p>&copy; Copyright. All rights reserved.</p>
-          <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+          <p>
             Copyright Ⓒ 2023 | All Rights Reserved | Page created to practice
             with{" "}
             <a
